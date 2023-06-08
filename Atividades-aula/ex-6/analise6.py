8fd81432f617bf95a3bceee2635ef44c3b3def70
Artur Uhlik Frohlich <artur.uhlik@gmail.com>
2023-06-07 22:44:10 -0300

    Atualização

---
+++ Atividades-aula/ex-6/analise6.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+simx, simy = np.loadtxt(sys.argv[1], unpack=True)
+
+simy = (simy - np.pi)**2
+
+zoom = 5000 #int(len(simx)/1.5)
+plt.scatter(simx[zoom:], simy[zoom:], s=.6, c='k')
+#plt.hlines(np.pi, simx[zoom], simx[-1], color='r', linewidth=0.8, linestyles='dashed')
+#plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('N')
+#plt.ylabel(r'$\pi$')
+plt.ylabel(r'$(4n/N - \pi)^{2}$')
+plt.grid(True, which='both')
+#plt.xlim(simx[zoom], simx[-1])
+#plt.ylim(0, simy[zoom])
+plt.tight_layout()
+plt.savefig('plot6.png', dpi=200)
+
---
+++ Atividades-aula/ex-7/mc_7.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define N 1000000  // Número de pontos
+#define TC 100		// "Tempo de correlação"
+
+int main(){
+    double uniform(double min, double max);
+    int seed = time(NULL);
+    srand(seed);
+    
+    // Criação do arquivo
+    char titulo1[20], titulo2[20];
+    sprintf(titulo1, "%i-N%i.txt", seed, N);
+	FILE *saida = fopen(titulo1, "w");
+    
+    // Definições para a dinâmica
+    int Nac = 0;			// Número de acertos
+    double rand1, rand2;
+    int t;
+    float razao;
+
+    for(t = 0; t < N; ++t){
+        rand1 = uniform(0., 1.);
+        rand2 = uniform(0., 1.);
+        if(rand2 < pow(sin(1./rand1), 2)) Nac++;
+        if(t%TC == 0){
+            razao = (4.*((float) Nac))/(t);
+            fprintf(saida, "%d\t%f\n", t, razao);
+        }
+    }
+    
+	// Roda o script de python
+	fclose(saida);
+	char comando[50];
+	sprintf(comando, "python3 analise6.py %s\n", titulo1);
+	printf("%s\n", comando);
+	system(comando);
+
+    return 0;
+}
+
+double uniform(double min, double max) {
+	/*
+	Função que gera um número aleatório em uma distribuição uniforme
+	Para int: [min, max)
+	*/
+	double random  = ((double) rand()) / RAND_MAX;
+	double range = (max - min) * random;
+	double n = range + min;	
+	
+	return n;
+}
+
