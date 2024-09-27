44a5f4c5f00c78365e1a74f19efa4f571a46092e
Artur-UF <artur.uhlik@gmail.com>
2024-09-27 12:07:40 -0300

    Atualização

--- Atividades-aula/ex-03/analise3.py
+++ Atividades-aula/ex-03/analise3.py
@@ -6,7 +6,7 @@ simx, simy = np.loadtxt(sys.argv[1], unpack=True)
 exatox, exatoy = np.loadtxt('bin100.dat', unpack=True)
 
 zoom = 30
-plt.scatter(simx[zoom:-zoom], simy[zoom:-zoom], s=3, c='k', label='Simulação')
+plt.scatter(simx[zoom:-zoom], simy[zoom:-zoom], s=3, c='k', label='Simulação', zorder=3)
 plt.plot(exatox[zoom:-zoom], exatoy[zoom:-zoom], linewidth=.8, label='Binomial')
 plt.legend()
 plt.xlabel('n(Bolas)')
